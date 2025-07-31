c64693bd671b9b2ce861f053d6519e88e39a88b4
Brian Clark <baclark@umd.edu>
2025-07-31 15:37:06 -0400

    more progress...

--- build_all.sh
+++ build_all.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 
 # TOPDIR="/cvmfs/myorg/software"
 TOPDIR="/tmp/spack-test-build"
+mkdir -p $TOPDIR
 BUILD_SCRIPT="./build.sh"
 OS_TAGS=("el9")
 
 for OS in "${OS_TAGS[@]}"; do
-    IMAGE="./${OS}.sif"
+    IMAGE="./osg_${OS}.sif"
 
     echo "[+] Building for $OS using image: $IMAGE"
     apptainer exec \
