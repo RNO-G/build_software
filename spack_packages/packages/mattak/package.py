4402226214a7dfc6cf44c62f5868da9ba29890f8
Brian Clark <baclark@umd.edu>
2025-08-02 14:16:30 -0400

    try changing build method

--- spack_packages/packages/mattak/package.py
+++ spack_packages/packages/mattak/package.py
@@ -15,17 +15,18 @@ class Mattak(CMakePackage):
         os.environ['RNO_G_INSTALL_DIR'] = str(prefix)
         os.environ['CC'] = spack_cc
         os.environ['CXX'] = spack_cxx
+        os.environ['CMAKE_FLAGS'] = '-DLIBRNO_G_SUPPORT=ON'
 
         print(f"Installing to prefix: {prefix}")
         print(f"Using CC: {os.environ['CC']}")
         print(f"Using CXX: {os.environ['CXX']}")
 
-        args = self.std_cmake_args + [
-            f'-DCMAKE_INSTALL_PREFIX={prefix}',
-            f'-DCMAKE_C_COMPILER={spack_cc}',
-            f'-DCMAKE_CXX_COMPILER={spack_cxx}',
-            '-DLIBRNO_G_SUPPORT=ON'
-        ]
-        cmake('.', *args)
+        # args = self.std_cmake_args + [
+        #     f'-DCMAKE_INSTALL_PREFIX={prefix}',
+        #     f'-DCMAKE_C_COMPILER={spack_cc}',
+        #     f'-DCMAKE_CXX_COMPILER={spack_cxx}',
+        #     '-DLIBRNO_G_SUPPORT=ON'
+        # ]
+        # cmake('.', *args)
         make()
         make('install')
