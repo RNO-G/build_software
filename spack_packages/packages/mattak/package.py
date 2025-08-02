93fac88a3d66f7c108a24ce213da6b2ae954b2b9
Brian Clark <baclark@umd.edu>
2025-08-02 13:29:14 -0400

    add mattak

---
+++ spack_packages/packages/mattak/package.py
@@ -0,0 +1,31 @@
+from spack.package import *
+import os
+
+class Mattak(CMakePackage):
+    """C++ library used by the RNO-G experiment."""
+
+    homepage = "https://github.com/RNO-G/mattak"
+    git      = "https://github.com/RNO-G/mattak.git"
+
+    version('main', branch="main")
+
+    depends_on('cmake', type='build')
+
+    def install(self, spec, prefix):
+        os.environ['RNO_G_INSTALL_DIR'] = str(prefix)
+        os.environ['CC'] = spack_cc
+        os.environ['CXX'] = spack_cxx
+
+        print(f"Installing to prefix: {prefix}")
+        print(f"Using CC: {os.environ['CC']}")
+        print(f"Using CXX: {os.environ['CXX']}")
+
+        args = self.std_cmake_args + [
+            f'-DCMAKE_INSTALL_PREFIX={prefix}',
+            f'-DCMAKE_C_COMPILER={spack_cc}',
+            f'-DCMAKE_CXX_COMPILER={spack_cxx}',
+            '-DLIBRNO_G_SUPPORT=ON'
+        ]
+        cmake('.', *args)
+        make()
+        make('install')
