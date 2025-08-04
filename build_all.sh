6def66e14a90b2abd7e01cd40c01b89ff22f27dd
Brian Clark <baclark@umd.edu>
2025-08-04 15:23:56 -0400

    Changes to make this work on the build host
    This is now ready to look the way it's supposed to on the build host.

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-# TOPDIR="/cvmfs/myorg/software"
-TOPDIR="/tmp/spack-test-build"
-mkdir -p $TOPDIR
+# the final real path where we want files to be installed
+INSTALL_DIR="/cvmfs/rnog.opensciencegrid.org/software"
+
+# the temporary spot where we are going to build and actually write files
+BUILD_DIR="/tmp/rnog_build"
+mkdir -p $BUILD_DIR
+
+# in cvmfs, we bind BUILD_DIR to INSTALL_DIR
+# so that we can temporarily write to BUILD_DIR,
+# but the install scripts *think* they are writing to INSTALL_DIR
+# so that we can rsync them to their final destination at the end
+# and everything looks alright
+
 BUILD_SCRIPT="./build.sh"
 OS_TAGS=("el9")
 
@@ -12,11 +22,10 @@ for OS in "${OS_TAGS[@]}"; do
 
     echo "[+] Building for $OS using image: $IMAGE"
     apptainer exec \
-        -B /tmp:/tmp \
-        -B /users:/users \
+        -B /tmp/rnog_scratch:/tmp \
+        -B "$BUILD_DIR":"$INSTALL_DIR" \
         -B /var:/var \
         -B "$PWD":"$PWD" \
-        -B "$TOPDIR":"$TOPDIR" \
         "$IMAGE" \
-        "$BUILD_SCRIPT" "$OS" "$TOPDIR"
+        "$BUILD_SCRIPT" "$OS" "$INSTALL_DIR"
 done
